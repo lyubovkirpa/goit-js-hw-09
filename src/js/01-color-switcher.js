ae7432abbd2111942893d66ab62b9c356747c8d1
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-03-02 01:22:47 +0200

    color switching

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
 
+let timerId = null;
+
+startButton.addEventListener('click', onChangeColorBackground);
+stopButton.addEventListener('click', onStopChangeColorBackground);
+
+function onChangeColorBackground(event) {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  event.currentTarget.disabled = true;
+}
+
+function onStopChangeColorBackground() {
+  clearInterval(timerId);
+  startButton.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
