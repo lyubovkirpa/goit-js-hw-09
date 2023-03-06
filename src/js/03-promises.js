07b34c5509ae52a2117b244bcb25ae2db3fad656
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-03-07 00:45:17 +0200

    Promise

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.from');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit {
+
+// }
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }
+      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
+  });
 }
