61b87a35041829b3d2ceafb987665b128fa30125
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-03-07 21:24:30 +0200

    add function to promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,26 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const formRef = document.querySelector('.from');
+const formRef = document.querySelector('.form');
 
 formRef.addEventListener('submit', onFormSubmit);
 
-// function onFormSubmit {
+function onFormSubmit(e) {
+  e.preventDefault();
 
-// }
+  const numberOfCalls = Number(formRef.amount.value);
+  const delayStep = Number(formRef.step.value);
+  let firstDelay = Number(formRef.delay.value);
+
+  for (let i = 0; i < numberOfCalls; i += 1) {
+    const position = i + 1;
+
+    createPromise(position, firstDelay)
+      .then(message => Notify.success(message))
+      .catch(error => Notify.failure(error));
+
+    firstDelay += delayStep;
+  }
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
